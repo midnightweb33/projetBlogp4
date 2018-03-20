57e6751f67635aea872dba4597b2cb1dbdef4603
midnightweb <midenet.k@gmail.com>
2018-03-20 16:14:33 +0100

    creation page par default

---
+++ Pages/Templates/default.php
@@ -0,0 +1,35 @@
+<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../../../favicon.ico">
+
+      <title>Starter Template for Bootstrap</title>
+
+      <!-- Bootstrap core CSS -->
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Custom styles for this template -->
+      <link href="starter-template.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Blog Forteroche</a>
+      </nav>
+
+      <main role="main" class="container">
+
+        <div class="starter-template">
+          <?= $content; ?>
+        </div>
+
+      </main><!-- /.container -->
+
+
+    </body>
+  </html>
