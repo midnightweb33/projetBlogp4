db3e2ebd2179ab94c8d15cfd87c04a7be513d2b5
midnightweb33 <36963149+midnightweb33@users.noreply.github.com>
2018-03-27 14:48:15 +0200

    Update Autoloader.php
    ajout de var dump

--- App/Autoloader.php
+++ App/Autoloader.php
@@ -1,16 +1,13 @@
 <?php 
 namespace App;	
-
 /**
  * Class autoloader
 */	
 	class autoloader{
-
 	/**
 	 * Enregistre notre autoloader
 	*/
 		static function register(){
-
 			spl_autoload_register(array(__CLASS__,'autoload'));
 		}
 	/**
@@ -18,14 +15,16 @@ namespace App;
 	 * @param $class string Le nom de la classe à charger
 	*/
 		static function autoload($class){
-
 			if(strrpos($class, __NAMESPACE__.'\\')===0){
-				$class=str_replace(__NAMESPACE__, '\\','', $class); 
+				var_dump($class);
+				$class=str_replace(__NAMESPACE__, '\\', $class);
+				var_dump($class);
 				$class=str_replace('\\', '/', $class);
+				var_dump($class);
+				var_dump(__DIR__.'/'.$class.'.php');
 				require __DIR__.'/'.$class.'.php';
+
 			}
 		}
-
 	}
-
 ?>
